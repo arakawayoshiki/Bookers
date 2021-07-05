50bd16aa6c77af9fddcda2bb10688109287dcbcf
arakawayoshiki <yamato4tennis@gmail.com>
2021-07-05 08:13:51 +0000

    bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,7 @@
 class BooksController < ApplicationController
+  def top
+  end
+
   def index
     @books = Book.all
   end
@@ -7,8 +10,21 @@ class BooksController < ApplicationController
   end
 
   def new
+    @book = Book.new
   end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
+  end 
 
   def edit
   end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
