ae43677e2243558f3bcf3c1a324073056d4f3bb2
arakawayoshiki <yamato4tennis@gmail.com>
2021-07-06 12:52:10 +0000

    bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+   
   end
 
   def show
@@ -17,7 +18,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to books_path(book.id)
+    redirect_to book_path(book.id)
   end
 
   def edit
