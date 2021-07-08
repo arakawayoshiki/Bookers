57d4482b689c5dd0c06affc191c4c3ef106ea6e5
arakawayoshiki <yamato4tennis@gmail.com>
2021-07-08 13:03:14 +0000

    bookers new

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,9 @@
 class BooksController < ApplicationController
-  def top
-  end
+ 
 
   def index
     @books = Book.all
-     @book = Book.new
+    @book = Book.new
   end
 
   def show
@@ -14,13 +13,13 @@ class BooksController < ApplicationController
 
 
   def create
-    book = Book.new(book_params)
-    if book.save
-    flash[:notice] = "Book was successfully created."  
-    redirect_to book_path(book.id)
-    
+    @book = Book.new(book_params)
+    if @book.save
+     flash[:notice] = "Book was successfully created."
+     redirect_to book_path(book.id)
     else
-    render :index
+      @books = Book.all
+     render :index
     end
   end
 
@@ -29,11 +28,15 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
     redirect_to book_path(book)
     flash[:notice] = "Book was successfully update."
+    else
+      @books = Book.all
+      render :edit
     end
+
   end
 
   def destroy
